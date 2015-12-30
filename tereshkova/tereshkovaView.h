5ae3cfb7f29155aa6c47b463d504ce0937952f6f
Justin Palmer <justin@labratrevenge.com>
2015-12-30 11:34:37 -0800

    Initial Commit

---
+++ tereshkova/tereshkovaView.h
@@ -0,0 +1,13 @@
+//
+//  tereshkovaView.h
+//  tereshkova
+//
+//  Created by Justin Palmer on 12/30/15.
+//  Copyright © 2015 Justin Palmer. All rights reserved.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface tereshkovaView : ScreenSaverView
+
+@end
