227beaa7d55b1428ca3389a1205b789f52fd08ae
Justin Palmer <justin@labratrevenge.com>
2016-01-02 11:55:09 -0800

    you can now use a named list of cities
     instead of explicitly having to supply a center lng/lat

--- tereshkova/index.js
+++ tereshkova/index.js
@@ -1,58 +1,73 @@
 (function() {
+
+  // Provide your own access token
   mapboxgl.accessToken = 'pk.eyJ1IjoiY2FnZWQiLCJhIjoiQjd2aXNGYyJ9.gr1QeGYwG1QYUW47I-DqaQ';
 
-  var cities = {
-    memphis: [-90.0489800999999, 35.149534299],
-    portland: [-122.6762071, 45.5234514990001]
+  // EDIT THIS TO ADD OR REMOVE LOCATIONS
+  // The format is name : [degree offset min, degree offset max]
+  //
+  // The offset is how far each random point falls from the center of the given
+  // location
+  var locations = {
+    'Portland, OR': [-0.2, 0.2],
+    'New York City': [-0.2, 0.2]
   }
 
-  var origin = cities.portland
+  var locationName,
+      origin,
+      olat,
+      olon,
+      offset = [0, 0],
 
-  var olat = origin[1],
-      olon = origin[0]
-
-  var easeInProperties = {
+  // Properties used when animating in on a location
+  easeInProperties = {
     zoom: 17,
     duration: 10000,
     pitch: 25,
     bearing: 45
-  }
+  },
 
-  var easeBackProperties = {
+  // Properties used when animating back from a location
+  easeBackProperties = {
     zoom: 13,
     duration: 5000,
     pitch: 0,
     bearing: 0
   }
 
-  var client = new MapboxClient(mapboxgl.accessToken)
-  var source = new mapboxgl.GeoJSONSource({ data: { type: 'FeatureCollection', features: [] } })
-
-  var map = new mapboxgl.Map({
-    container: 'map',
-    style: 'mapbox://styles/mapbox/satellite-hybrid-v8',
-    center: origin,
-    zoom: 13
-  })
+  var client = new MapboxClient(mapboxgl.accessToken),
+      source = new mapboxgl.GeoJSONSource({ data: { type: 'FeatureCollection', features: [] } }),
+      map = new mapboxgl.Map({
+        container: 'map',
+        style: 'mapbox://styles/mapbox/satellite-hybrid-v8',
+        zoom: 13
+      })
 
   map.on('style.load', function () {
     map.addSource('destinations', source)
     map.addLayer({
-        "id": "destinations",
-        "type": "circle",
-        "source": "destinations"
+        id: 'destinations',
+        type: 'circle',
+        source: 'destinations'
     })
-    transitionToRandomDestination()
+
+
+    locationName = shuffle(Object.keys(locations))[0]
+    client.geocodeForward(locationName, {}, function(err, res) {
+      var location = res.features[0]
+      origin = location.center
+      olat = origin[1]
+      olon = origin[0]
+
+      map.setCenter(origin)
+      offset = locations[locationName]
+      transitionToRandomDestination()
+    })
   })
 
-
-  function randomBetween(f, t) {
-    return (Math.random() * (f - t) + t)
-  }
-
   function transitionToRandomDestination() {
-    var distanceLat = randomBetween(-0.2, 0.2),
-        distanceLon = randomBetween(-0.2, 0.2)
+    var distanceLat = randomBetween(offset[0], offset[1]),
+        distanceLon = randomBetween(offset[0], offset[1])
 
     var dlon = (olon + distanceLon),
         dlat = (olat + distanceLat)
@@ -91,5 +106,26 @@
         speed: 0.3
       })
     })
+  }
+
+  // Generate a random value between two numbers
+  function randomBetween(f, t) {
+    return (Math.random() * (f - t) + t)
+  }
+
+  // Fisher–Yates shuffle.  See http://bost.ocks.org/mike/shuffle/
+  function shuffle(array) {
+    var m = array.length, t, i;
+    // While there remain elements to shuffle…
+    while (m) {
+      // Pick a remaining element…
+      i = Math.floor(Math.random() * m--);
+      // And swap it with the current element.
+      t = array[m];
+      array[m] = array[i];
+      array[i] = t;
+    }
+
+    return array;
   }
 })()
