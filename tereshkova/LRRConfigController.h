b1db87efdb1a1ab5edfe717b66281f35ae813079
Justin Palmer <justin@labratrevenge.com>
2016-01-02 11:09:14 -0800

    stub out config options

---
+++ tereshkova/LRRConfigController.h
@@ -0,0 +1,13 @@
+//
+//  LRRConfigController.h
+//  tereshkova
+//
+//  Created by Justin Palmer on 1/2/16.
+//  Copyright © 2016 Justin Palmer. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LRRConfigController : NSObject
+
+@end
---
+++ tereshkova/LRRConfigController.m
@@ -0,0 +1,13 @@
+//
+//  LRRConfigController.m
+//  tereshkova
+//
+//  Created by Justin Palmer on 1/2/16.
+//  Copyright © 2016 Justin Palmer. All rights reserved.
+//
+
+#import "LRRConfigController.h"
+
+@implementation LRRConfigController
+
+@end
