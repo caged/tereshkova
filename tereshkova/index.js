d1418617f00d35ca1c21906a15088f9a9aa31ab7
Justin Palmer <justin@labratrevenge.com>
2016-01-02 13:00:43 -0800

    add top 25 world cities, and top 50 us cities
    Feel free to tweak this to your liking.  This is my liking since I’m in
    the US.

--- tereshkova/index.js
+++ tereshkova/index.js
@@ -1,23 +1,95 @@
 (function() {
 
-  // Provide your own access token
-  mapboxgl.accessToken = 'pk.eyJ1IjoiY2FnZWQiLCJhIjoiQjd2aXNGYyJ9.gr1QeGYwG1QYUW47I-DqaQ';
+  // YOU SHOULD PROVIDE YOUR OWN TOKEN AS THIS PUBLIC ONE WILL LIKELY GO AWAWY
+  // Provide your own access token.  This is a default, public access token.
+  mapboxgl.accessToken = 'pk.eyJ1IjoiY2FnZWQiLCJhIjoiY2lpeGw0cmR0MDAyM3RvbHp5czNoa2ozNCJ9.ndGrZjq3n16egIziiEIxvw';
 
   // EDIT THIS TO ADD OR REMOVE LOCATIONS
-  // The format is name : [degree offset min, degree offset max]
-  //
-  // The offset is how far each random point falls from the center of the given
-  // location
-  var locations = {
-    'Portland, OR': [-0.2, 0.2],
-    'New York City': [-0.2, 0.2]
-  }
+  var locations = [
+    // Top 25 world cities based on population not in the US
+    "Tokyo, Japan",
+    "Mexico City, Distrito Federal, Mexico",
+    "Mumbai, Maharashtra, India",
+    "Sao Paulo, SRo Paulo, Brazil",
+    "Delhi, India",
+    "Shanghai, China",
+    "Kolkata, West Bengal, India",
+    "Dhaka, Bangladesh",
+    "Buenos Aires, Ciudad de Buenos Aires, Argentina",
+    "Karachi, Sind, Pakistan",
+    "Cairo, Al Qahirah, Egypt",
+    "Rio de Janeiro, Brazil",
+    "Osaka, Japan",
+    "Beijing, China",
+    "Manila, Metropolitan Manila, Philippines",
+    "Moscow, Moskva, Russia",
+    "Istanbul, Turkey",
+    "Paris, France",
+    "Seoul, Korea, South",
+    "Lagos, Nigeria",
+    "Jakarta, Jakarta Raya, Indonesia",
+    "Guangzhou, Guangdong, China",
+    "London, Westminster, United Kingdom",
+    "Lima, Peru",
+    "Tehran, Iran",
+    // Top 50 US cities based on population
+    "New York, New York",
+    "Los Angeles, California",
+    "Chicago, Illinois",
+    "Miami, Florida",
+    "Philadelphia, Pennsylvania",
+    "Dallas, Texas",
+    "Atlanta, Georgia",
+    "Boston, Massachusetts",
+    "Houston, Texas",
+    "Washington, D.C., District of Columbia",
+    "Detroit, Michigan",
+    "Phoenix, Arizona",
+    "San Francisco, California",
+    "Seattle, Washington",
+    "Irvine, California",
+    "San Diego, California",
+    "Minneapolis, Minnesota",
+    "San Juan, United States",
+    "Tampa, Florida",
+    "Denver, Colorado",
+    "Baltimore, Maryland",
+    "St. Louis, Missouri",
+    "Fort Lauderdale, Florida",
+    "Long Beach, California",
+    "Cleveland, Ohio",
+    "Portland, Oregon",
+    "Pittsburgh, Pennsylvania",
+    "Las Vegas, Nevada",
+    "San Bernardino, California",
+    "San Jose, California",
+    "Cincinnati, Ohio",
+    "Sacramento, California",
+    "Oakland, California",
+    "Virginia Beach, Virginia",
+    "San Antonio, Texas",
+    "Kansas City, Missouri",
+    "Ft. Worth, Texas",
+    "Indianapolis, Indiana",
+    "Milwaukee, Wisconsin",
+    "Orlando, Florida",
+    "Providence, Rhode Island",
+    "Columbus, Ohio",
+    "West Palm Beach, Florida",
+    "Raleigh, North Carolina",
+    "Austin, Texas",
+    "Birmingham, Alabama",
+    "Mesa, Arizona",
+    "Memphis, Tennessee",
+    "Norfolk, Virginia",
+    "Bridgeport, Connecticut"
+   ]
 
   var locationName,
       origin,
       olat,
       olon,
-      offset = [0, 0],
+      offset = [-0.2, 0.2],
 
   // Properties used when animating in on a location
   easeInProperties = {
@@ -56,16 +128,19 @@
     })
 
 
-    locationName = shuffle(Object.keys(locations))[0]
+    locationName = shuffle(locations)[0]
+
     client.geocodeForward(locationName, {}, function(err, res) {
       var location = res.features[0]
       origin = location.center
       olat = origin[1]
       olon = origin[0]
 
+      document.querySelector('.js-dest-info').style.display = 'block'
+      document.querySelector('.js-destination-name').innerHTML = locationName
+
       map.setCenter(origin)
-      offset = locations[locationName]
-      transitionToRandomDestination()
+      setTimeout(transitionToRandomDestination, 5000)
     })
   })
 
@@ -83,7 +158,6 @@
           dest = res.features[0].place_name,
           dist = turf.distance(originPoint, destPoint, 'miles')
 
-      document.querySelector('.js-dest-info').style.display = 'block'
       document.querySelector('.js-destination-distance').innerHTML = "Roughly " + Math.floor(dist) + " miles from the center of " + locationName
       document.querySelector('.js-destination-name').innerHTML = dest + " "
 
